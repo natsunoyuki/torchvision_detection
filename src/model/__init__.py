eb23ede24cb02587aa8ea4ea1c4f05fd6ce1cae9
Юукий Нацумэ <33706936+natsunoyuki@users.noreply.github.com>
2022-06-21 19:46:59 +0900

    Delete __init__.py

--- src/model/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-
-# Faster R-CNN training utilities.
-# https://github.com/PacktPublishing/Modern-Computer-Vision-with-PyTorch/blob/master/Chapter08/Training_Faster_RCNN.ipynb
-
-from . import train_utils
-from . import model
